c94852fa6f1669ad5ce020168b295a1546f3937e
Pickles-deth <bn21037@shibaura-it.ac.jp>
2026-09-27 00:50:00 +0900

    Update portable download URL to Google Drive link

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ APPS = [
 
 # Portable ZIP を GitHub Releases 等へ置いた後、
 # 実際のダウンロードURLへ変更してください。
-PORTABLE_DOWNLOAD_URL = "https://github.com/Pickles-deth/all-application/releases/download/v1.0/Nuclear_Volume_Analyzer_Portable_v1.0_RELEASE.zip"
+PORTABLE_DOWNLOAD_URL = "https://drive.google.com/file/d/1Fuq4SquczuRZbsWZHwgOTHFIa_VVOAu-/view?usp=sharing"
 
 
 # ============================================================
